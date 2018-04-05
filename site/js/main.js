3f93ac00b2ac9df2c2a3c3d47143bbae2932bd28
Julie Tennett <tennett.j@gmail.com>
2018-04-05 16:39:15 -0400

    add basic hover button functionality

--- site/js/main.js
+++ site/js/main.js
@@ -1,1 +1,27 @@
 import "../scss/main.scss"; //parcel doesn't understand importing stylesheets in html but it understands them in js
+import $ from "jquery";
+
+
+$(document).ready(function() {
+  buttonHover();
+});
+
+function buttonHover() {
+  let $sendButton = $('.send');
+  let $draftButton = $('.draft');
+  let $sendMsg = $('.msg-send');
+  let $draftMsg = $('.msg-draft');
+
+  $sendButton.on("mouseenter", function () {
+    $sendMsg.removeClass('js-hide');
+  });
+  $sendButton.on("mouseleave", function () {
+    $sendMsg.addClass('js-hide');
+  });
+  $draftButton.on("mouseenter", function () {
+    $draftMsg.removeClass('js-hide');
+  });
+  $draftButton.on("mouseleave", function () {
+    $draftMsg.addClass('js-hide');
+  });
+}
