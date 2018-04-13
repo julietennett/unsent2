08155f4c4ea570975294de340cc3dcd4f732225a
Julie Tennett <tennett.j@gmail.com>
2018-04-13 13:37:04 -0400

    all draft buttons cause draft text to appear

--- site/js/modules/draft.js
+++ site/js/modules/draft.js
@@ -6,16 +6,59 @@ class Draft {
   }
 
   viewDraft() {
-    let $draft01button = $('.draft1');
+    let $draft01 = $('.draft1');
+    let $draft02 = $('.draft2');
+    let $draft03 = $('.draft3');
+    let $draft04 = $('.draft4');
+    let $draft05 = $('.draft5');
+
     let $email01 = $('.email1');
+    let $email02 = $('.email2');
+    let $email03 = $('.email3');
+    let $email04 = $('.email4');
+    let $email05 = $('.email5');
+
     let $livetext = $('.live-type');
 
-    $draft01button.click( function() {
-      console.log('clicked!');
+    $draft01.click( function() {
       $livetext.css('display', 'none');
-      $email01.css('display', 'block');
-      $email01.css('opacity', 1);
-      $email01.css('transform', 'translateY(0)');
+      $email02.addClass('hide');
+      $email03.addClass('hide');
+      $email04.addClass('hide');
+      $email05.addClass('hide');
+      $email01.removeClass('hide');
+    })
+    $draft02.click( function() {
+      $livetext.css('display', 'none');
+      $email01.addClass('hide');
+      $email03.addClass('hide');
+      $email04.addClass('hide');
+      $email05.addClass('hide');
+      $email02.removeClass('hide');
+    })
+    $draft03.click( function() {
+      $livetext.css('display', 'none');
+      $email02.addClass('hide');
+      $email01.addClass('hide');
+      $email04.addClass('hide');
+      $email05.addClass('hide');
+      $email03.removeClass('hide');
+    })
+    $draft04.click( function() {
+      $livetext.css('display', 'none');
+      $email02.addClass('hide');
+      $email03.addClass('hide');
+      $email01.addClass('hide');
+      $email05.addClass('hide');
+      $email04.removeClass('hide');
+    })
+    $draft05.click( function() {
+      $livetext.css('display', 'none');
+      $email02.addClass('hide');
+      $email03.addClass('hide');
+      $email04.addClass('hide');
+      $email01.addClass('hide');
+      $email05.removeClass('hide');
     })
 
   }
