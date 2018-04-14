b589b3294a87d974b68f631cc9c8403e1193bd23
Julie Tennett <tennett.j@gmail.com>
2018-04-14 16:35:33 -0400

    backgrounds change per email

--- site/js/modules/background.js
+++ site/js/modules/background.js
@@ -1,1 +1,46 @@
+import $ from "jquery";
 
+const restBG = require('../../assets/restaurant/restaurant-bg.png');
+const parkBG = require('../../assets/park/park-bg.png');
+const cloudsBG = require('../../assets/clouds/clouds-bg.png');
+const howlBG = require('../../assets/howl/howl-bg.png');
+const sunBG = require('../../assets/sun/sun-bg.png');
+
+class Background {
+  constructor() {
+    this.changeBackground();
+   }
+
+  changeBackground() {
+    let $newEmail = $('.new-email-button');
+    let $draftButton = $('.draft-button');
+    let $bodyBg = $('.body__bg');
+    let bgNum = 0;
+
+    $newEmail.click(function () {
+      $bodyBg.css("background-image", `url(${restBG})`);
+      $bodyBg.addClass('js-bg-show');
+      bgNum += 1;
+    });
+    $draftButton.click( function() {
+      if (bgNum == 1) {
+        $bodyBg.css("background-image", `url(${parkBG})`);
+        bgNum += 1;
+      } else if (bgNum == 2) {
+        $bodyBg.css("background-image", `url(${cloudsBG})`);
+        bgNum += 1;
+      } else if (bgNum == 3) {
+        $bodyBg.css("background-image", `url(${howlBG})`);
+        bgNum += 1;
+      } else if (bgNum == 4) {
+        $bodyBg.css("background-image", `url(${sunBG})`);
+        bgNum += 1;
+      } else if (bgNum == 5) {
+        $bodyBg.removeClass('js-bg-show');
+      }
+    });
+  }
+
+}
+
+export default Background;
