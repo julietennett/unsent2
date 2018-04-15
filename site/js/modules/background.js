849ff316b015758aa2296d1ded05077b765cb476
Julie Tennett <tennett.j@gmail.com>
2018-04-14 23:37:13 -0400

    clean up js and howl light position

--- site/js/modules/background.js
+++ site/js/modules/background.js
@@ -14,9 +14,8 @@ class Background {
   changeBackground() {
     let $newEmail = $('.new-email-button');
     let $draftButton = $('.draft-button');
-    let $bodyBg = $('.body__bg');
+    let $body = $('body');
     let bgNum = 0;
-
     let $restContainer = $('.restaurant__container')
     let $parkContainer = $('.park__container');
     let $cloudContainer = $('.cloud__container');
@@ -25,35 +24,34 @@ class Background {
     let $img = $('img');
 
     $newEmail.click(function () {
-      $bodyBg.css("background-image", `url(${restBG})`);
-      $bodyBg.addClass('js-bg-show');
+      $body.css("background-image", `url(${restBG})`);
       $restContainer.children($img).addClass('js-show');
       bgNum += 1;
     });
     $draftButton.click( function() {
       if (bgNum == 1) {
         $restContainer.children($img).removeClass('js-show');
-        $bodyBg.css("background-image", `url(${parkBG})`);
+        $body.css("background-image", `url(${parkBG})`);
         $parkContainer.children($img).addClass('js-show');
         bgNum += 1;
       } else if (bgNum == 2) {
         $parkContainer.children($img).removeClass('js-show');
-        $bodyBg.css("background-image", `url(${cloudsBG})`);
+        $body.css("background-image", `url(${cloudsBG})`);
         $cloudContainer.children($img).addClass('js-show');
         bgNum += 1;
       } else if (bgNum == 3) {
         $cloudContainer.children($img).removeClass('js-show');
-        $bodyBg.css("background-image", `url(${howlBG})`);
+        $body.css("background-image", `url(${howlBG})`);
         $howlContainer.children($img).addClass('js-show');
         bgNum += 1;
       } else if (bgNum == 4) {
         $howlContainer.children($img).removeClass('js-show');
-        $bodyBg.css("background-image", `url(${sunBG})`);
+        $body.css("background-image", `url(${sunBG})`);
         $sunContainer.children($img).addClass('js-show');
         bgNum += 1;
       } else if (bgNum == 5) {
         $sunContainer.children($img).removeClass('js-show');
-        $bodyBg.css("background-image", "none");
+        $body.css("background-image", "none");
       }
     });
   }
