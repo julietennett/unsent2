cd05d28c1da47d16429d83586d4e856de05aa98f
Julie Tennett <tennett.j@gmail.com>
2018-04-16 21:22:42 -0400

    background is responsive

--- site/js/modules/background.js
+++ site/js/modules/background.js
@@ -14,7 +14,7 @@ class Background {
   changeBackground() {
     let $newEmail = $('.new-email-button');
     let $draftButton = $('.draft-button');
-    let $body = $('body');
+    let $background = $('.backgrounds__container');
     let bgNum = 0;
     let $restContainer = $('.restaurant__container')
     let $parkContainer = $('.park__container');
@@ -24,34 +24,34 @@ class Background {
     let $img = $('img');
 
     $newEmail.click(function () {
-      $body.css("background-image", `url(${restBG})`);
+      $background.css("background-image", `url(${restBG})`);
       $restContainer.children($img).addClass('js-show');
       bgNum += 1;
     });
     $draftButton.click( function() {
       if (bgNum == 1) {
         $restContainer.children($img).removeClass('js-show');
-        $body.css("background-image", `url(${parkBG})`);
+        $background.css("background-image", `url(${parkBG})`);
         $parkContainer.find('*').addClass('js-show');
         bgNum += 1;
       } else if (bgNum == 2) {
         $parkContainer.find('*').removeClass('js-show');
-        $body.css("background-image", `url(${cloudsBG})`);
+        $background.css("background-image", `url(${cloudsBG})`);
         $cloudContainer.children($img).addClass('js-show');
         bgNum += 1;
       } else if (bgNum == 3) {
         $cloudContainer.children($img).removeClass('js-show');
-        $body.css("background-image", `url(${howlBG})`);
+        $background.css("background-image", `url(${howlBG})`);
         $howlContainer.children($img).addClass('js-show');
         bgNum += 1;
       } else if (bgNum == 4) {
         $howlContainer.children($img).removeClass('js-show');
-        $body.css("background-image", `url(${sunBG})`);
+        $background.css("background-image", `url(${sunBG})`);
         $sunContainer.children($img).addClass('js-show');
         bgNum += 1;
       } else if (bgNum == 5) {
         $sunContainer.children($img).removeClass('js-show');
-        $body.css("background-image", "none");
+        $background.css("background-image", "none");
       }
     });
   }
